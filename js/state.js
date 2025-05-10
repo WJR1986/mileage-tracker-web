2501730fa979eb8993e6cedc4bd1f3b9c0ecbbaa
WJR1986 <willrichardson182@gmail.com>
2025-05-10 12:57:08 +0100

    attempt at modularisation v7

--- js/state.js
+++ js/state.js
@@ -4,14 +4,6 @@ export const REIMBURSEMENT_RATE_PER_MILE = 0.45;
 
 export let savedTripHistory = [];
 
-// Utility to reset global tripSequence and clear calculated data
-export function clearTripState() {
-  tripSequence = [];
-  delete tripSequence.calculatedTotalDistanceMiles;
-  delete tripSequence.calculatedTotalReimbursement;
-  delete tripSequence.calculatedLegDistances;
-}
-
 export let tripSequence = {
   addresses: [],
   calculatedTotalDistanceMiles: null,
