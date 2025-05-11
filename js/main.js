deac8314dd0f10bde9e1cf470e841f559c87acc9
WJR1986 <willrichardson182@gmail.com>
2025-05-11 16:59:08 +0100

    Fix attempt V6

--- js/main.js
+++ js/main.js
@@ -430,6 +430,6 @@ function initializeDatePickers() {
   });
 
   // History filters
-  flatpickr("#filter-start-date", { dateFormat: "d-m-Y", defaultDate: new Date() });
-  flatpickr("#filter-end-date", { dateFormat: "d-m-Y", defaultDate: new Date() });
+  flatpickr("#filter-start-date", { dateFormat: "d-m-Y", defaultDate: "select date" });
+  flatpickr("#filter-end-date", { dateFormat: "d-m-Y", defaultDate: "select date" });
 }
